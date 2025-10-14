ecb32962f039008259197cb630731ce0e96587e6
d-perado <sjm01065@naver.com>
2025-10-15 01:42:55 +0900

    lv 1. 계산기 완료

--- src/com/carculator/first/carculator.java
+++ src/com/carculator/first/carculator.java
@@ -4,23 +4,55 @@ import java.util.Scanner;
 
 public class carculator {
 
-    static boolean calcuratorPower = true;
-
     public static void main(String[] args) {
-        System.out.println(":::계산기를 이용해주셔서 감사합니다.:::");
-        System.out.println(":::종료를 원하시면 exit를 입력해주세요.:::");
 
         Scanner sc = new Scanner(System.in);
-
-        while (calcuratorPower) {
-            if (!sc.hasNext("exit")){
-
-
-            } else {
-                calcuratorPower = !calcuratorPower;
+        while (true) {
+            System.out.println(":::정수입력 하세요.:::");
+            String input1 = sc.nextLine();
+            if (input1.contains("exit")) {
                 System.out.println(":::계산기를 종료합니다.:::");
+                break;
             }
-        }
+            System.out.println(":::정수입력 하세요.:::");
+            String input2 = sc.nextLine();
+            if (input2.contains("exit")) {
+                System.out.println(":::계산기를 종료합니다.:::");
+                break;
+            }
+            System.out.println(":::사칙연산기호를 입력하세요.(+,-,/,*):::");
+            String input3 = sc.nextLine();
+            if (input3.contains("exit")) {
+                System.out.println(":::계산기를 종료합니다.:::");
+                break;
+            }
 
+            int num1 = 0;
+            int num2 = 0;
+
+            try {
+                num1 = Integer.parseInt(input1);
+                num2 = Integer.parseInt(input2);
+                switch (input3) {
+                    case "+":
+                        System.out.println(num1 + num2);
+                        break;
+                    case "*":
+                        System.out.println(num1 * num2);
+                        break;
+                    case "-":
+                        System.out.println(num1 - num2);
+                        break;
+                    case "/":
+                        System.out.println(num1 / num2);
+                        break;
+                    default:
+                        System.out.println(":::올바른 사칙연산기호를 입력해주세요.:::");
+                }
+            } catch (Exception e) {
+                System.out.println(":::숫자를 제대로 입력해주세요.:::");
+            }
+
+        }
     }
 }
