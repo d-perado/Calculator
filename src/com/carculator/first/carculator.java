b53c219746ce91f056755e8c3511b15890354c65
d-perado <sjm01065@naver.com>
2025-10-15 01:46:55 +0900

    lv 1. 나눗셈 분모 0추가.

--- src/com/carculator/first/carculator.java
+++ src/com/carculator/first/carculator.java
@@ -44,7 +44,11 @@ public class carculator {
                         System.out.println(num1 - num2);
                         break;
                     case "/":
-                        System.out.println(num1 / num2);
+                        if(num2==0){
+                            System.out.println(":::분모는 0이 될 수 없습니다.:::");
+                            break;
+                        }
+                        System.out.println((double)num1 / (double)num2);
                         break;
                     default:
                         System.out.println(":::올바른 사칙연산기호를 입력해주세요.:::");
